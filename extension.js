0230ee3d4640f7756c29c2d4f117cd3a5ff86a57
default-cybe <kaivalyaahir@pnahir.com>
2026-03-23 12:52:12 -0400

    Implement Receive Code command

--- extension.js
+++ extension.js
@@ -47,3 +47,39 @@ function activate() {
         vscode.window.showInformationMessage(team_code +" is your CodeShare Team Code which is Already copied in you clipboard")
     });
 
+    vscode.commands.registerCommand('codeshare.receivecode', async function () {
+        const editor = vscode.window.activeTextEditor;
+        if (!editor) {
+            return;
+        }
+        const res_team_code  = await vscode.window.showInputBox({
+            placeHolder: "Enter a Team Code"
+          });
+          
+          if (res_team_code) {
+            database.ref(`teamCode/${res_team_code}`).once("value", function(snapshot) {
+                const data = snapshot.val();
+                const coderes =data.copiedcode;
+                editor.edit(function (builder) {
+                    builder.insert(editor.selection.start, coderes);
+                    console.log("Code Received:", coderes);
+                    console.log(res_team_code);
+
+                    const statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 100);
+                    statusBarItem.text = "Team code (CodeShare): "+res_team_code;
+                    statusBarItem.tooltip = "Team Code";
+                    statusBarItem.show();
+
+                });
+            });
+          }
+    });    
+
+}
+
+function deactivate() {}
+
+module.exports = {
+    activate,
+    deactivate
+};
