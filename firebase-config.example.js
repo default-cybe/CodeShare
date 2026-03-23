bf05ce6b4a305f4908efeba31d02f0ccc23d095f
default-cybe <kaivalyaahir@pnahir.com>
2026-03-23 16:03:55 -0400

    Add Firebase config template

---
+++ firebase-config.example.js
@@ -0,0 +1,14 @@
+// Firebase configuration template.
+//
+// Copy this file to `firebase-config.js` and fill in the values from your own
+// Firebase project (Realtime Database must be enabled). `firebase-config.js` is
+// git-ignored so your credentials never get committed.
+module.exports = {
+    apiKey: "YOUR_API_KEY",
+    authDomain: "YOUR_PROJECT.firebaseapp.com",
+    databaseURL: "https://YOUR_PROJECT.firebaseio.com",
+    projectId: "YOUR_PROJECT",
+    storageBucket: "YOUR_PROJECT.appspot.com",
+    messagingSenderId: "YOUR_SENDER_ID",
+    appId: "YOUR_APP_ID"
+};
